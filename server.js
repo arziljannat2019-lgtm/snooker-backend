99e71e1bc13ed7b1b66231cd294b6181e1c1fce1
your-name <your-email>
2025-12-11 18:42:04 +0500

    CORS final fix

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const app = express();
 
 // -------------------- CORS FIX --------------------
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
 
-  // Handle preflight requests
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
 });
+
 // ---------------------------------------------------
 
 app.use(express.json());
