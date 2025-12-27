ef28c998c5b7527eb66766443940324a75886e32
your-name <your-email>
2025-12-27 14:30:21 +0500

    fix route callback undefined

--- routes/tables.js
+++ routes/tables.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const tablesController = require("../controllers/tablesController");
 
-// table start
-router.post("/start", tablesController.startTable);
+const {
+  startTable
+} = require("../controllers/tablesController");
 
-// table stop
-router.post("/stop", tablesController.stopTable);
+router.post("/start", startTable);
 
 module.exports = router;
